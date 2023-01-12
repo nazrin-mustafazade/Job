edf86181733e12b49cb3b87de23ffdcde09389e9
Fateh Mammadli <fatehshm@code.edu.az>
2023-01-12 23:12:41 +0400

    dbmodels-1

---
+++ JobMVC/JobMVC/Models/OrganizationTitle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace JobMVC.Models
+{
+	public class OrganizationTitle
+	{
+		public int Id { get; set; }
+
+		public int OrganizationId { get; set; }
+		public Organization Organization { get; set; }
+
+		public int TitleId { get; set; }
+		public Title Title { get; set; }
+	}
+}
+
---
+++ JobMVC/JobMVC/Models/Title.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace JobMVC.Models
+{
+	public class Title
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? Description { get; set; }
+
+        //
+
+        public ICollection<OrganizationTitle> OrganizationTitles { get; set; }
+
+    }
+}
+
